379e33d1b6c847c311230feee99a1afa0581e53a
Roberto Armas <robertoarmas95@gmail.com>
2016-12-21 00:30:00 -0500

    homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,63 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+<div class="main-header">
 
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-12 right">
+      <h3>ELECCIONES 2017</h3>
     </div>
+  </div>
+  <div class="row">
+    <!-- Nav tabs -->
+    <ul class="nav nav-tabs" role="tablist">
+      <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Candidatos Presidencia</a></li>
+      <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Candidatos Asamblea</a></li>
+    </ul>
+
+    <!-- Tab panes -->
+    <div class="tab-content">
+      <div role="tabpanel" class="tab-pane active" id="home">
+        <div class="row">
+          <!-- Init political card -->
+          <div class="politics-card col-md-6">
+            <div class="row">
+              <div class="col-md-8 col-lg-6">
+                <div class="binomial">
+                  <div class="president">
+                    <img src="{{asset('img/lenin-moreno.jpg')}}" alt="Lenin Moreno" class="img-circle" width="150px">
+                    <a href=""><span>VER PERFIL</span></a>
+                  </div>
+                  <div class="vicepresident">
+                    <img src="{{asset('img/jorge-glass.jpg')}}" alt="Lenin Moreno" class="img-circle" width="100px">
+                    <a href=""><span>VER PERFIL</span></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4 col-lg-6" style="text-align: center;">
+                  <div class="row">
+                    <img src="{{asset('img/alianza-pais.jpg')}}" class="political-party-logo" >
+                  </div>
+                  <div class="row">
+                    <label class="president-label">Lenin Moreno</label><br>
+                    <label class="politic-position">Candidato Presidencia</label>
+                  </div>
+                  <div class="row">
+                    <label class="vicepresident-label">Jorge Glas</label><br>
+                    <label class="politic-position">Candidato Vicepresidencia</label>
+                  </div>
+              </div>
+            </div>
+          </div>
+          <!-- End political card -->
+        </div>
+      </div>
+      <div role="tabpanel" class="tab-pane" id="profile">
+
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,15 +4,11 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
-
     <!-- Styles -->
     <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css" />
-
     <!-- Scripts -->
     <script>
         window.Laravel =
@@ -24,10 +20,10 @@
 </head>
 <body>
     <div id="app">
+      <div id="wrapper">
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
                 <div class="navbar-header">
-
                     <!-- Collapsed Hamburger -->
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
                         <span class="sr-only">Toggle Navigation</span>
@@ -35,10 +31,9 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        <img src="{{URL::to('img/radiografia-logo.png')}}" />
                     </a>
                 </div>
 
@@ -47,9 +42,12 @@
                     <ul class="nav navbar-nav">
                         &nbsp;
                     </ul>
-
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
+                      <li><a href="{{ url('/') }}">INICIO</a></li>
+                      <li><a href="{{ url('/candidatos') }}">CANDIDATOS</a></li>
+                      <li><a href="{{ url('/funcionarios') }}">FUNCIONARIOS PÚBLICOS</a></li>
+                      <li><a href="{{ url('/contacto') }}">CONTACTO</a></li>
                         <!-- Authentication Links -->
                         @if (Auth::guest())
                             <li><a href="{{ url('/login') }}">Login</a></li>
@@ -79,10 +77,92 @@
                 </div>
             </div>
         </nav>
+        <div id="content">
+          @yield('content')
+        </div>
+      </div>
 
-        @yield('content')
+        <div id="footer" class="container-fluid">
+          <div class="row">
+            <div id="footer-site-map" class="col-sm-12 col-md-8">
+              <div class="row">
+                <div class="col-md-4">
+                  <div class="row">
+                    <div class="footer-title-head">
+                      Nosotros
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="footer-item">
+                      <a href="{{URL::to('quienes-somos')}}">Quienes Somos</a>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="footer-item">
+                      <a href="{{URL::to('sumate-a-la-iniciativa')}}">Súmate a la iniciativa</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4">
+                  <div class="row">
+                    <div class="footer-title-head">
+                      Elecciones 2017
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="footer-item">
+                      <a href="{{URL::to('candidatos-presidencia')}}">Candidatos Presidencia</a>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="footer-item">
+                      <a href="{{URL::to('informacion-sobre-las-elecciones')}}">Información sobre las elecciones</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4">
+                  <div class="row">
+                    <div class="footer-title-head">
+                      Perfiles
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="footer-item">
+                        <a href="{{URL::to('funcionarios-publicos')}}">Funcionarios Públicos</a>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="footer-item">
+                        <a href="{{URL::to('buscador')}}">Buscador</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+
+              </div>
+            </div>
+            <div id="footer-sponsors" class="col-sm-12 col-md-4">
+              <div class="row">
+              Auspiciado por:
+              </div>
+              <div class="row">
+                <div class="col-sm-12 col-sm-offset-0 col-md-offset-6 col-md-6">
+                  <img src="{{asset('img/universidad-hemisferios-logo.png')}}" style="height: 32px;" class="footer-sponsor-img"/>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-12 col-sm-offset-0 col-md-offset-6 col-md-3">
+                  <img src="{{asset('img/udla-logo-footer.png')}}" class="footer-sponsor-img"/>
+                </div>
+                <div class="col-sm-12 col-sm-offset-0 col-md-3">
+                  <img src="{{asset('img/fcd-logo.png')}}"  style="height: 32px;" class="footer-sponsor-img"/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
-
     <!-- Scripts -->
     <script src="{{asset('js/app.js')}}"></script>
 </body>
