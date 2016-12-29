b0159b5c86aef1ee0e5f1da2c89e55d29a2a8381
Roberto Armas <robertoarmas95@gmail.com>
2016-12-29 00:07:41 -0500

    Profile

--- resources/views/administration/profiles/edit.blade.php
+++ resources/views/administration/profiles/edit.blade.php
@@ -4,7 +4,7 @@
 <!-- Content Header (Page header) -->
 <section class="content-header">
   <h1>
-    Usuarios
+    Perfil
     <small>Editar</small>
   </h1>
   <ol class="breadcrumb">
@@ -16,52 +16,275 @@
 
 <!-- Main content -->
 <section class="content">
-  <!-- /.row -->
-  <div class="row">
-    <div class="col-md-12">
-      <div class="box box-primary">
-        <div class="box-header with-border">
-          <h3 class="box-title">Ingresar Usuario</h3>
-        </div>
-        <form class="form-horizontal" action="{{URL::to(route('user.update',$user->id))}}" method="POST">
-          <input type="hidden" name="_token" value="{{ csrf_token() }}"/>
-          <input name="_method" type="hidden" value="PUT">
-          <div class="box-body">
-            <div class="form-group">
-              <label class="col-md-2 control-label">Nombre</label>
-              <div class="col-md-10">
-                <input type="text" name="name" class="form-control" value="{{ $user->name }}" required/>
-              </div>
+  <form action="{{URL::to(route('profile.update',$profile->id))}}" method="POST">
+    <input type="hidden" name="_token" value="{{ csrf_token() }}"/>
+    <input name="_method" type="hidden" value="PUT">
+    <!-- /.row -->
+    <div class="row">
+      <div class="col-md-12">
+        <button type="submit" class="btn btn-primary pull-right">Guardar</button>
+      </div>
+    </div>
+    <br>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="box box-primary">
+          <div class="box-header with-border">
+            <h3 class="box-title">Datos</h3>
+            <div class="box-tools pull-right">
+              <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+              </button>
             </div>
-            <div class="form-group">
-              <label class="col-md-2 control-label">Email</label>
-              <div class="col-md-10">
-                <input type="email" name="email" value="{{ $user->email }}" class="form-control" required/>
+          </div>
+          <div class="box-body">
+              <div class="form-group">
+                <div class="col-md-4">
+                  <label>Foto Perfil</label><br>
+                  <img class="img-circle" data-src="{{ (isset($profile->picture)) ? $profile->picture : 'holder.js/150x150' }}" />
+                  <input type="file" name="profile" placeholder="ingrese">
+                </div>
+                <div class="col-md-8">
+                    <label>Foto Detalle</label><br>
+                  <img class="img-thumbnail" data-src="{{ (isset($profile->picture)) ? $profile->picture : 'holder.js/200x150' }}" />
+                  <input type="file" name="profile" placeholder="ingrese">
+                </div>
               </div>
+              <br><br>
+              <div class="form-group row">
+                <div class="col-md-6">
+                  <label for="name">Nombre</label>
+                  <input type="text" class="form-control" name="name" value="{{$profile->person->name}}" id="name" placeholder="Ingrese el nombre" required>
+                </div>
+                <div class="col-md-6">
+                  <label for="email">Apellido</label>
+                  <input type="text" class="form-control" name="lastname"value="{{$profile->person->lastname}}"  id="lastname" placeholder="Ingrese el apellido" required>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="email">Descripción</label>
+                <textarea class="textarea" name="description" placeholder="Escriba la descripción aquí" style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
+              </div>
+              <div class="form-group row">
+                <div class="col-md-6">
+                  <label for="name">Facebook</label>
+                  <input type="text" class="form-control" name="facebook" value="{{$profile->person->name}}" id="name" placeholder="Ingrese el nombre">
+                </div>
+                <div class="col-md-6">
+                  <label for="email">Twitter</label>
+                  <input type="text" class="form-control" name="twitter"value="{{$profile->person->lastname}}"  id="lastname" placeholder="Ingrese el apellido">
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Datos Row -->
+    <div class="row">
+      <div class="col-md-12">
+        <div class="box box-success">
+          <div class="box-header with-border">
+            <h3 class="box-title">Timeline</h3>
+            <div class="box-tools pull-right">
+              <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+              </button>
+            </div>
+          </div>
+          <div class="box-body">
+            <div class="table-responsive">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th>Fecha inicio</th>
+                    <th>Fecha fin</th>
+                    <th>Título</th>
+                    <th>Tipo</th>
+                    <th>Descripción</th>
+                    <th>Acción</th>
+                  </tr>
+                </thead>
+                <tbody id="timeline-grid">
+                  @foreach($profile->person->timelines as $i => $timeline)
+                  <tr class="">
+                    <input type="hidden" name="timeline[{{$i}}]['id']" value="{{$timeline->id}}"/>
+                    <td class="startDate"><label>{{$timeline->start}}</label><input type="hidden" name="timeline[{{$i}}]['startDate']" value="{{$timeline->start}}"/></td>
+                    <td class="endDate"><label>{{$timeline->end}}</label><input type="hidden" name="timeline[{{$i}}]['endDate']" value="{{$timeline->end}}"/></td>
+                    <td class="title"><label>{{$timeline->shortDescription}}</label><input type="hidden" name="timeline[{{$i}}]['title']" value="{{$timeline->shortDescription}}"/></td>
+                    <td class="type"><label>{{$timeline->typeEvent}}</label><input type="hidden" name="timeline[{{$i}}]['type']" value="{{$timeline->typeEvent}}"/></td>
+                    <td class="description"><label>{{$timeline->description}}</label><input type="hidden" name="timeline[{{$i}}]['description']" value="{{$timeline->description}}"/></td>
+                    <td class="action"><button type="button" class="btn btn-danger btn-delete">Eliminar</button></td>
+                  </tr>
+                  @endforeach
+                </tbody>
+                <tfooter>
+                  <tr class="model-timeline hidden">
+                    <input type="hidden" name="id-model" value="-1"/>
+                    <td class="startDate"><label></label><input type="hidden" name="startDate-model" value="-1"/></td>
+                    <td class="endDate"><label></label><input type="hidden" name="endDate-model" value="-1"/></td>
+                    <td class="title"><label></label><input type="hidden" name="title-model" value="-1"/></td>
+                    <td class="type"><label></label><input type="hidden" name="type-model" value="-1"/></td>
+                    <td class="description"><label></label><input type="hidden" name="description-model" value="-1"/></td>
+                    <td class="action"><button type="button" class="btn btn-danger btn-delete">Eliminar</button></td>
+                  </tr>
+                </tfooter>
+              </table>
+            </div>
+              <div class="form-group row">
+                <div class="col-md-6">
+                  <label for="date">Fecha inicio</label>
+                  <input type="date" class="form-control" name="startDate-timeline" id="startDate">
+                </div>
+                <div class="col-md-6">
+                  <label for="name">Fecha fin</label>
+                  <input type="date" class="form-control" name="endDate-timeline" id="endDate">
+                </div>
+              </div>
+              <div class="form-group row">
+                <div class="col-md-6">
+                  <label for="email">Título</label>
+                  <input type="text" class="form-control" name="title-timeline" id="title" placeholder="Ingrese el título">
+                </div>
+                <div class="col-md-6">
+                  <label for="name">Tipo</label>
+                  <select id="type" name="type-timeline" class="form-control">
+                    <option value="1">Pública</option>
+                    <option value="2">Privada</option>
+                    <option value="3">Política</option>
+                  </select>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="email">Descripción</label>
+                <textarea id="description" class="textarea" name="description-timeline" placeholder="Escriba la descripción aquí" style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
+              </div>
+          </div>
+          <div class="box-footer">
+            <button id="add-to-timeline" type="button" class="btn btn-success">Agregar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Datos Row -->
+    <!-- Datos Row -->
+    <div class="row">
+      <div class="col-md-12">
+        <div class="box box-warning">
+          <div class="box-header with-border">
+            <h3 class="box-title">Transparencia</h3>
+            <div class="box-tools pull-right">
+              <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+              </button>
+            </div>
+          </div>
+          <div class="box-body">
+            <div class="form-horizontal">
             </div>
 
-            <div class="form-group">
-              <label class="col-md-2 control-label">Rol</label>
-              <div class="col-md-10">
-                <select name="role" class="form-control" required>
-                  @foreach ($roles as $role)
-                  @if($role->id == $user->role_id)
-                  <option value="{{$role->id}}" selected>{{$role->name}}</option>
-                  @else
-                  <option value="{{$role->id}}">{{$role->name}}</option>
-                  @endif
-                  @endforeach
-                </select>
-              </div>
-            </div>
+
           </div>
           <div class="box-footer">
             <button type="submit" class="btn btn-primary">Editar</button>
           </div>
-        </form>
+        </div>
       </div>
     </div>
-  </div>
+    <!-- Datos Row -->
+  </form>
   <!-- /.row -->
 </section>
+@endsection
+
+@section('scripts')
+<script>
+  $(function () {
+    //bootstrap WYSIHTML5 - text editor
+    $(".textarea").wysihtml5();
+  });
+</script>
+
+<script>
+
+
+  var $CONTAINER = $('#timeline-grid');
+
+  $('#add-to-timeline').click(function () {
+    var index = $('#timeline-grid').find('tr').length;
+    var $clone = $('.model-timeline').clone(true).removeClass('hidden model-timeline');
+    $clone.find('input').attr('name' , 'timeline['+index+']["id"]');
+    $clone.find('.startDate').find('input').attr('name' , 'timeline['+index+']["startDate"]');
+    $clone.find('.startDate').find('input').val($('#startDate').val());
+    $clone.find('.startDate').find('label').text($('#startDate').val());
+
+    $clone.find('.endDate').find('input').attr('name' , 'timeline['+index+']["endDate"]');
+    $clone.find('.endDate').find('input').val($('#endDate').val());
+    $clone.find('.endDate').find('label').text($('#endDate').val());
+
+    $clone.find('.title').find('input').attr('name' , 'timeline['+index+']["title"]');
+    $clone.find('.title').find('input').val($('#title').val());
+    $clone.find('.title').find('label').text($('#title').val());
+
+    $clone.find('.type').find('input').attr('name' , 'timeline['+index+']["type"]');
+    $clone.find('.type').find('input').val($('#type').val());
+    $clone.find('.type').find('label').text($('#type option:selected').text());
+
+    $clone.find('.description').find('input').attr('name' , 'timeline['+index+']["description"]');
+    $clone.find('.description').find('input').val($('#type').val());
+    $clone.find('.description').find('label').text($('#description').val());
+
+    $CONTAINER.append($clone);
+
+
+    sortInputsTimeline();
+
+  });
+
+  $('.btn-delete').click(function(){
+    var $this = $(this);
+    var divToDelete = $($this.context.parentElement.parentElement);
+    divToDelete.remove();
+
+    sortInputsTimeline();
+  });
+
+  function sortInputsTimeline()
+  {
+    var childrens = $CONTAINER.find('tr').clone(true);
+    var dates =[];
+    for (var i=0; i<childrens.length;i++)
+    {
+      var child=$(childrens[i]);
+        dates.push( child.find('.startDate').find('label').text());
+    }
+    dates.sort();
+    $CONTAINER.find('tr').remove();
+    var childrensSorted = [];
+    for (var i=0; i<dates.length;i++)
+    {
+      childrensSorted.push(extract(dates[i],childrens));
+    }
+
+    for (var i = 0; i < childrensSorted.length; i++) {
+      var $clone = $(childrensSorted[i]).clone(true);
+      $clone.find('input').attr('name' , 'timeline['+i+']["id"]');
+      $clone.find('.startDate').find('input').attr('name' , 'timeline['+i+']["startDate"]');
+      $clone.find('.endDate').find('input').attr('name' , 'timeline['+i+']["endDate"]');
+      $clone.find('.title').find('input').attr('name' , 'timeline['+i+']["title"]');
+      $clone.find('.type').find('input').attr('name' , 'timeline['+i+']["type"]');
+      $clone.find('.description').find('input').attr('name' , 'timeline['+i+']["description"]');
+      $CONTAINER.append($clone);
+    }
+  }
+
+  function extract(date,array)
+  {
+    for (var i = 0; i < array.length; i++) {
+      var child=$(array[i]);
+      if(child.find('.startDate').find('label').text() === date)
+      {
+        return array.splice(i, 1)[0];
+      }
+    }
+  }
+
+  sortInputsTimeline();
+</script>
 @endsection
