c68207d94f899f104d9deb7879f5be230194b465
Roberto Armas <robertoarmas95@gmail.com>
2017-01-26 19:47:44 -0500

    integrate framework

--- app/Repositories/Repository.php
+++ app/Repositories/Repository.php
@@ -3,58 +3,10 @@
 namespace App\Repositories;
 
 use Auth;
+use UmpactoSoluciones\Tools\Repositories\Repository as BaseRepository;
 
-abstract class Repository
+abstract class Repository extends BaseRepository
 {
 
-  protected $model;
-
-  protected function clean($string) {
-     $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
-     return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
-  }
-
-  public function nameGenerator($string)
-  {
-    $name = "";
-    $string = mb_strtolower($string,'UTF-8');
-    $name = $this->clean($string);
-    return $name;
-  }
-
-  public function create($data)
-  {
-      $data['user_id'] = Auth::user()->id;
-      if($this->model->create($data) != null)
-      {
-          return true;
-      }else {
-        return false;
-      }
-  }
-
-  public function update($id, $data)
-  {
-    $m = $this->model->find($id);
-    $data['user_id'] = Auth::user()->id;
-    return $m->update($data);
-
-  }
-
-  public function delete($id)
-  {
-    $m =  $this->model->find($id);
-    return $m->delete();
-  }
-
-  public function find($id)
-  {
-    return $this->model->find($id);
-  }
-
-  public function all()
-  {
-    return $this->model->all();
-  }
 
 }
